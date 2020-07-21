5daae019d470c4343245a700c8933e1bad3902e2
joey <tchuang@sps-program.com>
2020-07-21 16:43:39 +0800

    finish week4 tdd all tests

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -23,28 +23,87 @@ public final class FindMeetingQuery {
 
     Collection<String> attendees = request.getAttendees();
     long duration = request.getDuration();
-    Collection<TimeRange> freeTimeRange = Arrays.asList(TimeRange.WHOLE_DAY);
+    List<TimeRange> freeTimeRange = Arrays.asList(TimeRange.WHOLE_DAY);
+    ArrayList<TimeRange> possibleTimeRange = new ArrayList<>();
+
+    // no attendees
     if (attendees.size() == 0){
       return freeTimeRange;
     }
+
+    // impossible arrange a meeting
     if (duration > TimeRange.WHOLE_DAY.duration()){
       return Arrays.asList();
     }
     
-    for (Event e: events){
-      Collection<TimeRange> tmp = new ArrayList<TimeRange>();
-      TimeRange meeting = e.getWhen();
-      for (TimeRange t: freeTimeRange){
-        if (t.overlaps(meeting)){
-          System.out.print(t);
-          TimeRange t1 = TimeRange.fromStartEnd(t.start(), meeting.start(), false);
-          TimeRange t2 = TimeRange.fromStartEnd(meeting.end(), t.end(), false);
-          tmp.add(t1);
-          tmp.add(t2);
-        }
-      freeTimeRange = tmp;
+    // split time range by meeting
+    for (Event event: events){
+      Set<String> requiredAttendees = new HashSet<>(event.getAttendees());
+      requiredAttendees.retainAll(attendees);
+      if (!requiredAttendees.isEmpty()){
+        freeTimeRange = splitTimeRange(freeTimeRange, event.getWhen());
       }
     }
-    return freeTimeRange;
+
+    // check duration is enough
+    for (TimeRange range: freeTimeRange){
+      if (range.duration() >= duration){
+        possibleTimeRange.add(range);
+      }
+    }
+    return possibleTimeRange;
+  }
+
+  public List<TimeRange> splitTimeRange(Collection<TimeRange> freeTimeRange, TimeRange meeting){
+    ArrayList<TimeRange> ranges = new ArrayList<>(freeTimeRange);
+    int idx = 0;
+    for (TimeRange range: ranges){
+      if (range.overlaps(meeting)){
+        // process the flag
+        boolean meetingStartEarlier = true;
+        boolean meetingEndEarlier = true;
+        if (range.start() < meeting.start()){
+          meetingStartEarlier = false;
+        }
+        if (range.end() <= meeting.end()){
+          meetingEndEarlier = false;
+        }
+
+        // split case by case
+        // Case 1
+        //    |-----| range
+        // |-----|    meeting
+        if (meetingStartEarlier && meetingEndEarlier){
+          TimeRange free = TimeRange.fromStartEnd(meeting.end(), range.end(), false);
+          ranges.set(idx, free);
+        }
+        // Case 2
+        // |-----|    range
+        //    |-----| meeting
+        else if (!meetingStartEarlier && !meetingEndEarlier){
+          TimeRange free = TimeRange.fromStartEnd(range.start(), meeting.start(), false);
+          ranges.set(idx, free);
+        }
+        // Case 3
+        //   |---|    range
+        // |-------|  meeting
+        else if (meetingStartEarlier && !meetingEndEarlier){
+          ranges.remove(idx);
+          idx = idx - 1;
+        }
+        // Case 4
+        // |-------|  range
+        //   |---|    meeting
+        else {
+          TimeRange freeLeft = TimeRange.fromStartEnd(range.start(), meeting.start(), false);
+          TimeRange freeRight = TimeRange.fromStartEnd(meeting.end(), range.end(), false);
+          ranges.set(idx, freeLeft);
+          ranges.add(idx+1, freeRight);
+          break;
+        }
+      }
+      idx = idx + 1;
+    }
+    return ranges;
   }
 }
